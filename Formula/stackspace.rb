8bc1742a7c6e56aad59cb52e750f860a0366bd80
Rick Whalley <rickwhalley@icloud.com>
2026-05-10 10:19:35 +0100

    stackspace v0.2.0

--- Formula/stackspace.rb
+++ Formula/stackspace.rb
@@ -1,9 +1,9 @@
 class Stackspace < Formula
   desc "Generic CLI for creating and managing meta-repository workspaces"
   homepage "https://github.com/StackCube/stackspace"
-  version "0.1.0"
-  url "https://updates.stackcube.dev/stackspace/v0.1.0/stackspace-darwin-arm64"
-  sha256 "dbbd46cdb3f53dad560f05686a37b219ef738dddcd6f387a60e8e00a45fc0438"
+  version "0.2.0"
+  url "https://updates.stackcube.dev/stackspace/v0.2.0/stackspace-darwin-arm64"
+  sha256 "0e88928b5ad24e17ccfa040a63a6ab73b28076de1145867b49643d520ade9d73"
 
   depends_on arch: :arm64
   depends_on :macos
