c38fea03d71199f4e97a0f1b68e36d5b58d0ca6c
Rick Whalley <rickwhalley@icloud.com>
2026-05-17 21:34:20 +0100

    stackpaas v0.6.0

--- Formula/stackpaas.rb
+++ Formula/stackpaas.rb
@@ -1,9 +1,9 @@
 class Stackpaas < Formula
   desc "Engineer-facing CLI for the StackCube PaaS"
   homepage "https://stackcube.dev"
-  version "0.5.0"
-  url "https://updates.stackcube.dev/stackpaas/v0.5.0/stackpaas-darwin-arm64"
-  sha256 "3c3a2e2d872c40684bb415e39cf524e892d8b2e1546cf84171c840d60c882996"
+  version "0.6.0"
+  url "https://updates.stackcube.dev/stackpaas/v0.6.0/stackpaas-darwin-arm64"
+  sha256 "2d42cd28a8ca0e8ccb34eafde62885e258a2f7b62d4d673e33bf315acefe72a7"
 
   depends_on arch: :arm64
   depends_on :macos
--- Formula/stackpaasd.rb
+++ Formula/stackpaasd.rb
@@ -1,9 +1,9 @@
 class Stackpaasd < Formula
   desc "Daemon for the StackCube PaaS"
   homepage "https://stackcube.dev"
-  version "0.5.0"
-  url "https://updates.stackcube.dev/stackpaasd/v0.5.0/stackpaasd-darwin-arm64"
-  sha256 "f1c24fa2a52bb958615f69510a34ff34917c17be84cacf449d1f01e25927e067"
+  version "0.6.0"
+  url "https://updates.stackcube.dev/stackpaasd/v0.6.0/stackpaasd-darwin-arm64"
+  sha256 "9534f441f5382179539672f4e90c0d42cbe69f908e8b40c0506c731680d19514"
 
   depends_on arch: :arm64
   depends_on :macos
